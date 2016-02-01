f64963a28f29cd46cb96c65a025857f3b3f21930
nengzhizhi <445954717@qq.com>
2016-02-02 00:27:23 +0800

    create model relation

---
+++ server/boot/createDefaultData.js
@@ -0,0 +1,52 @@
+var async = require('async');
+module.exports = function (app) {
+  var loopback = require('loopback');
+
+  var LiveStreamUrl = app.models.LiveStreamUrl;
+  var Shop = app.models.Shop;
+
+  // var shops = [
+  //   { name: '大悦城', shopId: 001 }
+  // ]
+  //
+  // if (!!Shop && !!LiveStreamUrl) {
+  //   shops.forEach(function (shop) {
+  //     Shop.findOrCreate({
+  //       where: { name: shop.name }
+  //     }, shop, function (err, createdShop, created) {
+  //       console.log(err, createdShop);
+  //       var url = {};
+  //       url.shopId = createdShop.id;
+  //       url.urls = [{
+  //         type: 'flv',
+  //         value: 'http://xx.flv'
+  //       }];
+  //
+  //       LiveStreamUrl.create(url, function (err, instance) {
+  //         console.log(err, instance);
+  //       });
+  //     })
+  //   })
+  // }
+
+  // async.waterfall([
+  //   function (next) {
+  //     Shop.find({
+  //       where: { name: '大悦城' }
+  //     }, next);
+  //   }, function (shop, next) {
+  //     console.log(shop);
+  //
+  //     LiveStreamUrl.find({ "filter": {
+  //       "where": { "shopId": shop.id },
+  //       "include": { "relation": "shop" }
+  //     }}, next)
+  //   }
+  // ], function (err, result) {
+  //   console.log(err, result);
+  // })
+
+  LiveStreamUrl.find({include: 'shop'}, function (err, result) {
+    console.log(err, result);
+  })
+}
