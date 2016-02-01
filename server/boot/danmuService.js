9a8c607c7bbd2e39df047c167a10d18c4e1ec0c5
nengzhizhi <nengzhizhi@gmail.com>
2016-02-02 00:40:04 +0800

    modify danmuService.js

--- server/boot/danmuService.js
+++ server/boot/danmuService.js
@@ -1,5 +1,10 @@
 var socketIO = require('socket.io');
+var PORT = 3003;
 
 module.exports = function(app){
+  var io = new socketIO();
+  io.on('connection', function (socket) {
 
+  })
+  io.listen(PORT);
 }
