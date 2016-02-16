8941a72838358d55b124504475d4e8d5b466a937
nengzhizhi <nengzhizhi@126.com>
2016-02-16 20:07:15 +0800

    relation

--- server/boot/createDefaultData.js
+++ server/boot/createDefaultData.js
@@ -3,58 +3,37 @@ module.exports = function (app) {
   var loopback = require('loopback');
 
   var LiveStreamUrl = app.models.LiveStreamUrl;
+  var LiveStream = app.models.LiveStream;
   var Shop = app.models.Shop;
 
   var shops = [
     { name: '大悦城' }
   ]
 
-  // if (!!Shop && !!LiveStreamUrl) {
-  //   shops.forEach(function (shop) {
-  //     Shop.findOrCreate({
-  //       where: { name: shop.name }
-  //     }, shop, function (err, createdShop, created) {
-  //       console.log(err, createdShop);
-  //       var url = {};
-  //       url.shopId = createdShop.id;
-  //       url.urls = [{
-  //         type: 'flv',
-  //         value: 'http://xx.flv'
-  //       }];
-  //
-  //       LiveStreamUrl.create(url, function (err, instance) {
-  //         console.log(err, instance);
-  //       });
-  //     })
-  //   })
-  // }
+  async.waterfall([
+    // function createShop(next){
+    //   Shop.create({ name: '万达' }, next);
+    // },
+    // function createShopStream(shop, next) {
+    //   shop.liveStreams.create({
+    //     type: 'flv',
+    //     value: 'http://xxxx.flv'
+    //   }, next);
+    // }
+    function getShop(next){
+      Shop.findOne({
+        where: { name: "万达" },
+        include: 'liveStreams'
+      }, next);
+    }
+    // function getStream(next){
+    //   LiveStream.findOne({
+    //     include: 'shop'
+    //   }, next)
+    // }
 
-  // async.waterfall([
-  //   function (next) {
-  //     Shop.find({
-  //       where: { name: '大悦城' }
-  //     }, next);
-  //   }, function (shop, next) {
-  //     console.log(shop);
-  //
-  //     LiveStreamUrl.find({ "filter": {
-  //       "where": { "shopId": shop.id },
-  //       "include": { "relation": "shop" }
-  //     }}, next)
-  //   }
-  // ], function (err, result) {
-  //   console.log(err, result);
-  // })
 
-  // Shop.findOne({
-  //     where: { name: '大悦城' }
-  //   },
-  //   function (err, result) {
-  //     console.log(err, result);
-  //   }
-  // )
-
-  // LiveStreamUrl.find({include: 'shop'}, function (err, result) {
-  //   console.log(err, result);
-  // })
+  ], function (err, result) {
+    console.log(err, result);
+  })
 }
